f5935e415fdebf21f720a3f772ebb7bb7161e86b
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2021-04-14 10:24:24 +0700

    membuat field taget dan status

--- database/migrations/2021_04_14_031810_create_todolist_project.php
+++ database/migrations/2021_04_14_031810_create_todolist_project.php
@@ -15,6 +15,8 @@ class CreateTodolistProject extends Migration
     {
         Schema::create('todolists', function (Blueprint $table) {
             $table->id();
+            $table->string('target');
+            $table->integer('status');
             $table->timestamps();
         });
     }
